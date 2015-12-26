d064e7d9fc4e3c4711f1eb82a1850c785f5e9ee8
amymd <to5ro10@gmail.com>
2015-12-26 20:40:54 +0900

    スレッド一覧画面のレイアウトをテーブル表示にした

--- bbs/resources/views/bbs/index.blade.php
+++ bbs/resources/views/bbs/index.blade.php
@@ -1,25 +1,34 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="col-xs-8 col-xs-offset-2">
-    @if(Session::has('message'))
-    <div class="bg-info">
-        <p>{{ Session::get('message') }}</p>
-    </div>
-    @endif
-
-    {!! link_to('bbs/create', 'スレッド新規作成', ['class' => 'btn btn-primary']) !!}
-
-    @foreach($threads as $thread)
-
-    <h2>{!! link_to("bbs/show/{$thread->id}", $thread->title) !!}</h2>
-    <small>投稿日：{{ date("Y年 m月 d日",strtotime($thread->created_at)) }}</small>
-    <small>投稿者：{{ $thread->user }}</small>
-    <p>{{ $thread->description }}</p>
-    <p>コメント数：{{ $thread->comment_count }}</p>
-    <hr />
-    @endforeach
-
+<h4>スレッド一覧</h4>
+@if(Session::has('message'))
+<div class="chip">
+    <p>{{ Session::get('message') }}</p>
 </div>
+@endif
+
+{!! link_to('bbs/create', 'スレッド新規作成', ['class' => 'waves-effect waves-light btn']) !!}
+
+<table class="highlight bordered">
+    <thead>
+        <tr>
+            <th data-field="datetime">投稿日</th>
+            <th data-field="thread">スレッド名</th>
+            <th data-field="commentNum">投稿数</th>
+            <th data-field="user">投稿者</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($threads as $thread)
+        <tr>
+            <td>{{ date("Y年 m月 d日",strtotime($thread->created_at)) }}</td>
+            <td>{!! link_to("bbs/show/{$thread->id}", $thread->title) !!}</td>
+            <td>{{ $thread->comment_count }}</td>
+            <td>{{ $thread->user }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 
 @stop
