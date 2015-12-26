fdba18620af6863a494c37a6f931f05f97e1ef84
amymd <to5ro10@gmail.com>
2015-12-26 22:40:36 +0900

    スレッド詳細画面のレイアウトを修正した

--- bbs/resources/views/bbs/show.blade.php
+++ bbs/resources/views/bbs/show.blade.php
@@ -1,51 +1,67 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="col-xs-8 col-xs-offset-2">
-    <h2>{{ $thread->title}}</h2>
-    <small>投稿日：{{ date("Y年 m月 d日",strtotime($thread->created_at)) }}</small>
-    <small>投稿者：{{ $thread->user }}</small>
-    <p>{{ $thread->description }}</p>
-
-    <hr/>
-
-    <h3>コメント一覧</h3>
-    @foreach($thread->comments as $comment)
-    <h4>{{ $comment->user }}</h4>
-    <p>{{ $comment->comment }}</p>
-    @endforeach
-
-    <h3>コメント投稿</h3>
-    @if(Session::has('message'))
-    <div class="bg-info">
-        <p>{{ Session::get('message') }}</p>
-    </div>
-    @endif
-
-    @foreach($errors->all() as $message)
-    <p class="bg-danger">{{ $message }}</p>
-    @endforeach
-
-    {!! Form::open(['url' => 'comments'], array('class' => 'form')) !!}
-
-    <div class="form-group">
-      {!! Form::label('user', '投稿者') !!}
-      {!! Form::text('user', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-      {!! Form::label('comment', '本文') !!}
-      {!! Form::text('comment', null, ['class' => 'form-control']) !!}
-    </div>
-
-    {!! Form::hidden('thread_id', $thread->id) !!}
-
-    <div class="form-group">
-      {!! Form::submit('投稿する', ['class' => 'btn btn-primary form-control']) !!}
-    </div>
-
-    {!! Form::close() !!}
-
+@if(Session::has('message'))
+<div class="chip chip-info white-text">
+    {{ Session::get('message') }}
+    <i class="material-icons">close</i>
 </div>
+@endif
+
+@foreach($errors->all() as $message)
+<div class="chip chip-error white-text">
+    {{ $message }}
+    <i class="material-icons">close</i>
+</div>
+@endforeach
+
+<div class="card-panel">
+    <p class="flow-text">{{ $thread->title}}</p>
+    投稿者：{{ $thread->user }}
+    <div class="right">
+        投稿日：{{ date("Y年 m月 d日",strtotime($thread->created_at)) }}
+    </div>
+    <hr />
+    <div class="card-content">
+        <p>{{ $thread->description }}</p>
+    </div>
+</div>
+
+@foreach($thread->comments as $comment)
+<div class="card-panel">
+    投稿者：{{ $comment->user }}
+    <div class="right">
+        投稿日：{{ date("Y年 m月 d日",strtotime($comment->created_at)) }}
+    </div>
+    <hr />
+    <div class="card-content">
+        <p>{{ $comment->comment }}</p>
+    </div>
+</div>
+@endforeach
+
+{!! Form::open(['url' => 'comments'], array('class' => 'col s12')) !!}
+
+<div class="row">
+    <div class="input-field col s6">
+        <i class="material-icons prefix">account_circle</i>
+        {!! Form::label('user', '投稿者') !!}
+        {!! Form::text('user', null, ['class' => 'validate']) !!}
+    </div>
+</div>
+
+<div class="row">
+    <div class="input-field col s12">
+        <i class="material-icons prefix">mode_edit</i>
+        {!! Form::label('comment', '本文') !!}
+        {!! Form::text('comment', null, ['class' => 'validate']) !!}
+    </div>
+</div>
+
+{!! Form::hidden('thread_id', $thread->id) !!}
+
+{!! Form::submit('投稿する', ['class' => 'btn waves-effect waves-light']) !!}
+
+{!! Form::close() !!}
 
 @stop
--- bbs/resources/views/layouts/app.blade.php
+++ bbs/resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <!-- materialize -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="/css/materialize_custom.css">
 
 </head>
 <body>
